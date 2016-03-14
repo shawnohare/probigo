5f13c924c7229450b35b40fbea2265d26a6de5b0
Shawn O'Hare <ohare.shawn@gmail.com>
2016-03-13 19:43:58 -0700

    Introduce Data struct.
    The `Store` interface now deals primarily with the `Data` struct.

--- store.go
+++ store.go
@@ -1,20 +1,52 @@
 package probigo
 
-// Back implementations represent the minimal functionality that a key-value
-// store (or data structure server) should provide to back the probabilistic
+// Define some data structure types typically supported by Store interfaces.
+const (
+	// Top level element
+	Top = iota + 1
+	Hash
+	Set
+	OrderedSet
+	HyperLogLog
+)
+
+// Data is an object that a Data Store manipulates.
+type Data struct {
+	// Element is the underlying object pushed to a probabilistic data structure.
+	Element []byte
+	// Key associated to the data.  This is usually computed by a probabilistic
+	// data structure, and not set by the user.
+	Key string
+	// Expiry indicates when, in seconds, the element should expire. (Optional)
+	Expiry int
+	// Type represents what type of data structure the store should use to
+	// store this element.
+	Type int
+}
+
+// Store implementations represent the minimal functionality that a data
+// structure store should provide to back the probabilistic
 // data structures defined in this package.  For simplicity, all set
 // elements are assumed to be of type []byte.
 //
 // Generally speaking, the user is not expected to provide their own
 // implementation.  By default, the probabilistic data structures are
 // Redis backed.  User defined backings can be useful for unit testing.
-type Back interface {
-	// Get the value for the input key.
-	Get(string) ([]byte, error)
-	// Set the given key to the value, with an optional expiration in seconds.
-	Set(string, []byte, ...int) error
-	// Get the value associated to the key and set it to the input value
-	// with an optional expiration in seconds.
-	GetSet(string, []byte, ...int) ([]byte, error)
-	Exists(string) (bool, error)
+type Store interface {
+	Exists(*Data) (bool, error)
+	Get(*Data) (*Data, error)
+	Set(*Data) error
+	Del(*Data) error
 }
+
+// type Store interface {
+// 	// Get the value for the input key.
+// 	Get(string) ([]byte, error)
+// 	// Set the given key to the value, with an optional expiration in seconds.
+// 	Set(string, []byte, int) error
+// 	// Get the value associated to the key and set it to the input value
+// 	// with an optional expiration in seconds.
+// 	GetSet(string, []byte, int) ([]byte, error)
+// 	Delete(string) error
+// 	Exists(string) (bool, error)
+// }
