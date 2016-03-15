6039ff6d6545d09ef6fc7af31a53e7bb7623a1e2
Shawn O'Hare <ohare.shawn@gmail.com>
2016-03-15 16:20:19 -0700

    Simplify Store interface.
    The `Store` interface is defined now only by a single `Do` method,
    mimicing the redigo Redis connection.

--- store.go
+++ store.go
@@ -1,52 +1,25 @@
 package probigo
 
-// Define some data structure types typically supported by Store interfaces.
-const (
-	// Top level element
-	Top = iota + 1
-	Hash
-	Set
-	OrderedSet
-	HyperLogLog
+import (
+	"strings"
+
+	"github.com/garyburd/redigo/redis"
 )
 
-// Data is an object that a Data Store manipulates.
-type Data struct {
-	// Element is the underlying object pushed to a probabilistic data structure.
-	Element []byte
-	// Key associated to the data.  This is usually computed by a probabilistic
-	// data structure, and not set by the user.
-	Key string
-	// Expiry indicates when, in seconds, the element should expire. (Optional)
-	Expiry int
-	// Type represents what type of data structure the store should use to
-	// store this element.
-	Type int
-}
-
-// Store implementations represent the minimal functionality that a data
-// structure store should provide to back the probabilistic
-// data structures defined in this package.  For simplicity, all set
-// elements are assumed to be of type []byte.
-//
-// Generally speaking, the user is not expected to provide their own
-// implementation.  By default, the probabilistic data structures are
-// Redis backed.  User defined backings can be useful for unit testing.
+// Store abstracts the redigo Redis connection.
 type Store interface {
-	Exists(*Data) (bool, error)
-	Get(*Data) (*Data, error)
-	Set(*Data) error
-	Del(*Data) error
+	Do(string, ...interface{}) (interface{}, error)
 }
 
-// type Store interface {
-// 	// Get the value for the input key.
-// 	Get(string) ([]byte, error)
-// 	// Set the given key to the value, with an optional expiration in seconds.
-// 	Set(string, []byte, int) error
-// 	// Get the value associated to the key and set it to the input value
-// 	// with an optional expiration in seconds.
-// 	GetSet(string, []byte, int) ([]byte, error)
-// 	Delete(string) error
-// 	Exists(string) (bool, error)
-// }
+func keyExists(store Store, key string) (bool, error) {
+	return redis.Bool(store.Do("EXISTS", key))
+}
+
+// isFieldExpired checks to see if the top-level key:field:ex id exists.
+// Key is typically the identifier for some data structure, such as a hash
+// hyperloglog, etc... If the composite top level key does not exist,
+// then the field is expired.
+func isFieldExpired(store Store, key string, field string) (bool, error) {
+	topkey := strings.Join([]string{key, field, "ex"}, ":")
+	return keyExists(store, topkey)
+}
